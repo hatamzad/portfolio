eb0e051c837ea8889830e8271c31d64047773e2e
hatamzad <amirhatamzad@gmail.com>
2019-03-09 13:00:42 -0800

    This is my first  commit

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
